abdf4c3bddaca9788f0a07cd5fba36b0c49c769a
Phoupraw <467934387@qq.com>
2023-02-01 20:02:16 +0800

    提取交换物品方法

--- src/main/java/phoupraw/mcmod/createsdelight/block/PanBlock.java
+++ src/main/java/phoupraw/mcmod/createsdelight/block/PanBlock.java
@@ -7,20 +7,15 @@ import com.simibubi.create.foundation.block.ITE;
 import net.fabricmc.fabric.api.transfer.v1.context.ContainerItemContext;
 import net.fabricmc.fabric.api.transfer.v1.fluid.FluidConstants;
 import net.fabricmc.fabric.api.transfer.v1.fluid.FluidStorage;
-import net.fabricmc.fabric.api.transfer.v1.fluid.base.SingleFluidStorage;
-import net.fabricmc.fabric.api.transfer.v1.item.InventoryStorage;
-import net.fabricmc.fabric.api.transfer.v1.item.PlayerInventoryStorage;
 import net.fabricmc.fabric.api.transfer.v1.storage.StorageUtil;
-import net.fabricmc.fabric.api.transfer.v1.storage.base.SingleVariantStorage;
 import net.fabricmc.fabric.api.transfer.v1.transaction.Transaction;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.ShapeContext;
 import net.minecraft.block.entity.BlockEntityType;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.inventory.SimpleInventory;
 import net.minecraft.sound.SoundCategory;
-import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
@@ -32,8 +27,10 @@ import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 import phoupraw.mcmod.createsdelight.api.ConstantSingleFluidStorage;
 import phoupraw.mcmod.createsdelight.api.ConstantSingleItemStorage;
+import phoupraw.mcmod.createsdelight.api.LivingEntityStorage;
 import phoupraw.mcmod.createsdelight.block.entity.PanBlockEntity;
 import phoupraw.mcmod.createsdelight.registry.MyBlockEntityTypes;
+import phoupraw.mcmod.createsdelight.storage.BlockingTransportedStorage;
 
 import java.util.function.Predicate;
 //TODO 支架
@@ -42,6 +39,29 @@ public class PanBlock extends Block implements ITE<PanBlockEntity> {
 
     public static <T> Predicate<@Nullable T> always() {
         return t -> true;
+    }
+
+    public static boolean swapItem(LivingEntity living, Hand hand, BlockingTransportedStorage blockStorage) {
+        boolean success = false;
+        long toBlock;
+        try (var transaction = Transaction.openOuter()) {
+            var livingStorage = LivingEntityStorage.of(living);
+            long capacity = blockStorage.getCapacity();
+            var tempStorage = new ConstantSingleItemStorage(capacity);
+            long toLiving = StorageUtil.move(blockStorage, tempStorage, always(), capacity, transaction);
+            toBlock = StorageUtil.move(livingStorage.get(hand), blockStorage, always(), capacity, transaction);
+            if (StorageUtil.move(tempStorage, livingStorage, always(), capacity, transaction) == toLiving) {
+                transaction.commit();
+                success = toLiving > 0 || toBlock > 0;
+            }
+        }
+        if (toBlock > 0) {
+            var transported = blockStorage.getTransported();
+            transported.insertedFrom = living.getHorizontalFacing();
+            transported.prevBeltPosition = .25f;
+            transported.beltPosition = .25f;
+        }
+        return success;
     }
 
     public PanBlock(Settings settings) {
@@ -61,8 +81,8 @@ public class PanBlock extends Block implements ITE<PanBlockEntity> {
         var pan = world.getBlockEntity(pos, MyBlockEntityTypes.PAN).orElseThrow();
         try (var transaction = Transaction.openOuter()) {
             var handStorage = FluidStorage.ITEM.find(player.getStackInHand(hand), ContainerItemContext.ofPlayerHand(player, hand));
-            if (handStorage!=null) {
-                var panStorage = pan.getTankBehaviour().getCapability();
+            if (handStorage != null) {
+                var panStorage = pan.getTank().getCapability();
                 var tempStorage = new ConstantSingleFluidStorage(FluidConstants.BOTTLE);
                 var amount = StorageUtil.move(panStorage, tempStorage, always(), Long.MAX_VALUE, transaction);
                 StorageUtil.move(handStorage, panStorage, always(), Long.MAX_VALUE, transaction);
@@ -72,27 +92,7 @@ public class PanBlock extends Block implements ITE<PanBlockEntity> {
                 }
             }
         }
-        boolean success = false;
-        try (var transaction = Transaction.openOuter()) {
-            var playerStorage = PlayerInventoryStorage.of(player);
-            var panStorage = pan.getTransportedBehaviour().getStorage();
-            var tempStorage = new ConstantSingleItemStorage(1);
-            var amount = StorageUtil.move(panStorage, tempStorage, always(), Long.MAX_VALUE, transaction);
-            StorageUtil.move(playerStorage.getHandSlot(hand), panStorage, always(), Long.MAX_VALUE, transaction);
-            if (StorageUtil.move(tempStorage, playerStorage, always(), Long.MAX_VALUE, transaction) == amount) {
-                transaction.commit();
-                success = true;
-            }
-        }
-        if (success) {
-            var transported = pan.getTransportedBehaviour().getStorage().getTransported();
-            transported.insertedFrom = player.getHorizontalFacing();
-            transported.prevBeltPosition = .25f;
-            transported.beltPosition = .25f;
-            pan.sendData();
-            return ActionResult.SUCCESS;
-        }
-        return ActionResult.FAIL;
+        return swapItem(player, hand, pan.getItem().getStorage()) ? ActionResult.SUCCESS : ActionResult.FAIL;
     }
 
     @Override
@@ -102,9 +102,7 @@ public class PanBlock extends Block implements ITE<PanBlockEntity> {
             if (random.nextInt(10) == 0) {
                 world.playSound(pos.getX() + 0.5, pos.getY(), pos.getZ() + 0.5, SoundsRegistry.BLOCK_SKILLET_SIZZLE.get(), SoundCategory.BLOCKS, 0.4F, random.nextFloat() * 0.2F + 0.9F, false);
             }
-//            if (random.nextInt(5) == 0) {
             world.addParticle(ParticleTypesRegistry.STEAM.get(), pos.getX() + 0.5 + (random.nextDouble() * 0.4 - 0.2), pos.getY() + 0.1, pos.getZ() + 0.5 + (random.nextDouble() * 0.4 - 0.2), 0.0, random.nextBoolean() ? 0.015 : 0.005, 0.0);
-//            }
         }
     }
 
