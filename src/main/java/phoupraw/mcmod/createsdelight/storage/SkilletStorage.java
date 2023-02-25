e42afe30f1b28a96f04bbca2f55137bfedda08cd
Phoupraw <467934387@qq.com>
2023-02-25 11:11:42 +0800

    适应MC1.18.2农夫乐事无煎锅。

--- src/main/java/phoupraw/mcmod/createsdelight/storage/SkilletStorage.java
+++ src/main/java/phoupraw/mcmod/createsdelight/storage/SkilletStorage.java
@@ -1,7 +1,6 @@
 package phoupraw.mcmod.createsdelight.storage;
 
 import com.nhoryzon.mc.farmersdelight.entity.block.SkilletBlockEntity;
-
 import net.fabricmc.fabric.api.transfer.v1.item.ItemVariant;
 import net.fabricmc.fabric.api.transfer.v1.item.base.SingleStackStorage;
 import net.minecraft.item.ItemStack;
@@ -10,33 +9,32 @@ import phoupraw.mcmod.createsdelight.api.FarmersDelightWrappers;
 
 import java.util.Objects;
 /**
- * @see FarmersDelightWrappers#storageOf(SkilletBlockEntity)
- */
+ @see FarmersDelightWrappers#storageOf(SkilletBlockEntity) */
 public class SkilletStorage extends SingleStackStorage {
-	private final SkilletBlockEntity skillet;
+    private final SkilletBlockEntity skillet;
 
-	public SkilletStorage(SkilletBlockEntity skillet) {this.skillet = skillet;}
+    public SkilletStorage(SkilletBlockEntity skillet) {this.skillet = skillet;}
 
-	@Override
-	protected ItemStack getStack() {
-		return skillet.getStoredStack();
-	}
+    @Override
+    protected ItemStack getStack() {
+        return skillet.getStoredStack();
+    }
 
-	@Override
-	protected void setStack(ItemStack stack) {
-		skillet.getInventory().setStack(0, stack);
-	}
+    @Override
+    protected void setStack(ItemStack stack) {
+        skillet.getInventory().setStack(0, stack);
+    }
 
-	@Override
-	protected boolean canInsert(ItemVariant itemVariant) {
-		return FarmersDelightWrappers.findRecipe(SkilletStorage.class, Objects.requireNonNull(skillet.getWorld()), RecipeType.CAMPFIRE_COOKING, itemVariant) != null;
-	}
+    @Override
+    protected boolean canInsert(ItemVariant itemVariant) {
+        return FarmersDelightWrappers.findRecipe(SkilletStorage.class, Objects.requireNonNull(skillet.getWorld()), RecipeType.CAMPFIRE_COOKING, itemVariant) != null;
+    }
 
-	@Override
-	protected void onFinalCommit() {
-		super.onFinalCommit();
-		var itemStack = getStack();
-		setStack(ItemStack.EMPTY);
-		skillet.addItemToCook(itemStack, null);
-	}
+    @Override
+    protected void onFinalCommit() {
+        super.onFinalCommit();
+        var itemStack = getStack();
+        setStack(ItemStack.EMPTY);
+        skillet.addItemToCook(itemStack, null);
+    }
 }
