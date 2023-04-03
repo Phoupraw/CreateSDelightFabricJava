fc25ddabf824b0c75a21d83c81c693b9620c35ff
Phoupraw <467934387@qq.com>
2023-04-03 10:10:46 +0800

    BigCakeBlock

--- src/main/java/phoupraw/mcmod/createsdelight/block/JellyBeansCakeBlock.java
+++ src/main/java/phoupraw/mcmod/createsdelight/block/JellyBeansCakeBlock.java
@@ -1,36 +1,23 @@
 package phoupraw.mcmod.createsdelight.block;
 
-import com.mojang.datafixers.util.Pair;
-import com.nhoryzon.mc.farmersdelight.registry.EffectsRegistry;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
-import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.ShapeContext;
-import net.minecraft.entity.effect.StatusEffectInstance;
-import net.minecraft.entity.effect.StatusEffects;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.state.StateManager;
-import net.minecraft.util.ActionResult;
-import net.minecraft.util.Hand;
 import net.minecraft.util.function.BooleanBiFunction;
-import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.util.shape.VoxelShapes;
 import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
-import net.minecraft.world.WorldAccess;
 import net.minecraft.world.WorldView;
-import phoupraw.mcmod.createsdelight.item.StatusEffectsBlockItem;
 import phoupraw.mcmod.createsdelight.registry.MyBlocks;
-import phoupraw.mcmod.createsdelight.registry.MyStatusEffects;
 
 import java.util.List;
 
 import static net.minecraft.state.property.Properties.AGE_3;
-public class JellyBeansCakeBlock extends Block {
+public class JellyBeansCakeBlock extends BigCakeBlock {
     public static final List<VoxelShape> OUTLINE_SHAPES = List.of(
       createCuboidShape(2, 0, 2, 14, 10, 14),
       VoxelShapes.union(
@@ -59,26 +46,6 @@ public class JellyBeansCakeBlock extends Block {
 
     public JellyBeansCakeBlock(Settings settings) {
         super(settings);
-        setDefaultState(getDefaultState().with(AGE_3, 0));
-    }
-
-    @Override
-    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
-        super.appendProperties(builder);
-        builder.add(AGE_3);
-    }
-
-    @SuppressWarnings("deprecation")
-    @Override
-    public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
-        StatusEffectsBlockItem.eat(world, pos, state, hit.getPos(), player, 5, 0.5f, List.of(Pair.of(new StatusEffectInstance(MyStatusEffects.SATIATION, 1, 3), 1f), Pair.of(new StatusEffectInstance(StatusEffects.SATURATION, 2, 0), 1f), Pair.of(new StatusEffectInstance(EffectsRegistry.COMFORT.get(), 20 * 60 * 2, 0), 1f)));
-        int bites = state.get(AGE_3);
-        if (bites < 3) {
-            world.setBlockState(pos, state.cycle(AGE_3));
-        } else {
-            world.setBlockState(pos, MyBlocks.JELLY_BEANS.getDefaultState());
-        }
-        return ActionResult.SUCCESS;
     }
 
     @SuppressWarnings("deprecation")
@@ -93,15 +60,13 @@ public class JellyBeansCakeBlock extends Block {
         return COLLISION_SHAPES.get(state.get(AGE_3));
     }
 
-    @SuppressWarnings("deprecation")
     @Override
     public boolean canPlaceAt(BlockState state, WorldView world, BlockPos pos) {
         return !VoxelShapes.matchesAnywhere(world.getBlockState(pos.down()).getSidesShape(world, pos.down()).getFace(Direction.UP), COLLISION_SHAPES.get(0), BooleanBiFunction.ONLY_SECOND);
     }
 
-    @SuppressWarnings("deprecation")
     @Override
-    public BlockState getStateForNeighborUpdate(BlockState state, Direction direction, BlockState neighborState, WorldAccess world, BlockPos pos, BlockPos neighborPos) {
-        return direction == Direction.DOWN && !this.canPlaceAt(state, world, pos) ? Blocks.AIR.getDefaultState() : super.getStateForNeighborUpdate(state, direction, neighborState, world, pos, neighborPos);
+    public void onFinalEat(World world, BlockPos blockPos, BlockState blockState) {
+        world.setBlockState(blockPos, MyBlocks.JELLY_BEANS.getDefaultState());
     }
 }
