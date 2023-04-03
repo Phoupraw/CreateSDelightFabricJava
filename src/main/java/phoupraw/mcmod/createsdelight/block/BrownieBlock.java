d8bc36ee5c18fa38850066d641c263a82688aa7f
Phoupraw <467934387@qq.com>
2023-04-03 10:14:14 +0800

    SmallCakeBlock

--- src/main/java/phoupraw/mcmod/createsdelight/block/BrownieBlock.java
+++ src/main/java/phoupraw/mcmod/createsdelight/block/BrownieBlock.java
@@ -1,33 +1,17 @@
 package phoupraw.mcmod.createsdelight.block;
 
-import com.mojang.datafixers.util.Pair;
-import com.nhoryzon.mc.farmersdelight.registry.EffectsRegistry;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
-import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.ShapeContext;
-import net.minecraft.entity.effect.StatusEffectInstance;
-import net.minecraft.entity.effect.StatusEffects;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.state.StateManager;
-import net.minecraft.util.ActionResult;
-import net.minecraft.util.Hand;
 import net.minecraft.util.function.BooleanBiFunction;
-import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.util.shape.VoxelShapes;
 import net.minecraft.world.BlockView;
-import net.minecraft.world.World;
-import net.minecraft.world.WorldAccess;
 import net.minecraft.world.WorldView;
-import phoupraw.mcmod.createsdelight.item.StatusEffectsBlockItem;
-import phoupraw.mcmod.createsdelight.registry.MyStatusEffects;
-
-import java.util.List;
-public class BrownieBlock extends Block {
+public class BrownieBlock extends SmallCakeBlock {
     public static final VoxelShape SHAPE = VoxelShapes.union(
       createCuboidShape(4, 0, 4, 12, 14, 12),
       createCuboidShape(5, 14, 5, 11, 16, 11));
@@ -40,26 +24,12 @@ public class BrownieBlock extends Block {
         super(settings);
     }
 
-    @Override
-    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
-        super.appendProperties(builder);
-    }
-
-    @SuppressWarnings("deprecation")
-    @Override
-    public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
-        StatusEffectsBlockItem.eat(world, pos, state, hit.getPos(), player, 5, 0.5f, List.of(Pair.of(new StatusEffectInstance(MyStatusEffects.SATIATION, 1, 3), 1f), Pair.of(new StatusEffectInstance(StatusEffects.SATURATION, 2, 0), 1f), Pair.of(new StatusEffectInstance(EffectsRegistry.COMFORT.get(), 20 * 60 * 2, 0), 1f)));
-        world.breakBlock(pos, false, player);
-        return ActionResult.SUCCESS;
-    }
-
     @SuppressWarnings("deprecation")
     @Override
     public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
         return SHAPE;
     }
 
-    @SuppressWarnings("deprecation")
     @Override
     public boolean canPlaceAt(BlockState state, WorldView world, BlockPos pos) {
 //        if (world.isClient()) {
@@ -68,11 +38,5 @@ public class BrownieBlock extends Block {
 //            CreateSDelight.LOGGER.info(VoxelShapes.matchesAnywhere(SHAPE,face, BooleanBiFunction.ONLY_SECOND));
 //        }
         return !VoxelShapes.matchesAnywhere(world.getBlockState(pos.down()).getSidesShape(world, pos.down()).getFace(Direction.UP), SHAPE, BooleanBiFunction.ONLY_SECOND);//BlockHelper.hasBlockSolidSide(world.getBlockState(pos.down()), world, pos.down(), Direction.UP);
-    }
-
-    @SuppressWarnings("deprecation")
-    @Override
-    public BlockState getStateForNeighborUpdate(BlockState state, Direction direction, BlockState neighborState, WorldAccess world, BlockPos pos, BlockPos neighborPos) {
-        return direction == Direction.DOWN && !this.canPlaceAt(state, world, pos) ? Blocks.AIR.getDefaultState() : super.getStateForNeighborUpdate(state, direction, neighborState, world, pos, neighborPos);
     }
 }
